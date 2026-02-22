74d623c75c600831f0820db8d77cae51a3762327
Keshav Singhal <221880058+keshav-builds@users.noreply.github.com>
2026-02-22 13:41:09 +0530

    Resume Upd

--- src/config/Resume.ts
+++ src/config/Resume.ts
@@ -1,3 +1,3 @@
 export const resumeConfig = {
-  url: 'https://drive.google.com/file/d/1wiSbUaAfkV505Ykw7xWQJY9ipsEmWpzQ/preview',
+  url: 'https://drive.google.com/file/d/1AF_0CIXvr1ro2mSY0f0FXjdFc7o0IJu4/preview',
 };
